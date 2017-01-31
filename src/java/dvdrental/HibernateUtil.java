201fb72ff0c8d7c623a11f9e950a06132b87b79a
derz41 <andersonm@chc.edu>
2017-01-31 09:18:01 -0500

    initial setup of project with JSF and Hibernate

---
+++ src/java/dvdrental/HibernateUtil.java
@@ -0,0 +1,38 @@
+
+package dvdrental;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+/**
+ * Hibernate Utility class with a convenient method to get Session Factory
+ * object.
+ *
+ * @author Matt
+ */
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+    
+    static {
+        try {
+            // Create the SessionFactory from standard (hibernate.cfg.xml) 
+            // config file.
+            Configuration configuration = new Configuration();
+            configuration.configure();
+            
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+            sessionFactory = new Configuration().configure().buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            // Log the exception. 
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
