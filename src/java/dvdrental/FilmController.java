11dbcfa2b7b7fdad8d10479834f01867204fecd4
derz41 <andersonm@chc.edu>
2017-02-16 09:18:33 -0500

    finished implementation for the previous and next buttons. Also started the brows page for view link on index page but not fully finished. Add film still not fully functional

--- src/java/dvdrental/FilmController.java
+++ src/java/dvdrental/FilmController.java
@@ -23,8 +23,14 @@ public class FilmController implements Serializable {
     FilmHelper helper;
     
     private int recordCount;
-    
     private int pageSize = 10;
+    //this field is needed to get the movie selected in the index.xhtml
+    // it maps to the components in the browse.xhtml
+    private Film selected;
+    //these fields map to the components int he browse.xhtml
+    String language;
+    String actors;
+    String category;
 
     /**
      * Creates a new instance of FilmController
@@ -35,6 +41,8 @@ public class FilmController implements Serializable {
         startId = 0;
         
         recordCount = helper.getNumberFilms();
+        
+        
         
     }
 
@@ -79,7 +87,73 @@ public class FilmController implements Serializable {
     public void setPageSize(int pageSize) {
         this.pageSize = pageSize;
     }
+    //this method will return true if it makes sense to add the next button
+    public boolean isHasNextPage() {
+        if (startId + pageSize < recordCount){
+            return true;
+        }
+        return false;
+    }
+    //this method will return true if it makes sense to add the Previous button
+    public boolean isHasPreviousPage(){
+        if(startId - pageSize > 0){
+            return true;
+        }
+        return false;
+    }
+
+    public Film getSelected() {
+        if(selected == null){
+            selected = new Film();
+        }
+        return selected;
+    }
+
+    public void setSelected(Film selected) {
+        this.selected = selected;
+    }
+
+    public String getLanguage() {
+        int LangID = selected.getLanguageByLanguageId().getLanguageId().intValue();
+        language = helper.getLangByID(LangID);
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getActors() {
+        List actors = helper.getActorsByID(selected.getFilmId());
+        //convery list to string
+        StringBuilder cast = new StringBuilder();
+        for(int i = 0; i < actors.size(); i++){
+            Actor actor = (Actor) actors.get(i);
+            cast.append(actor.getFirstName());
+            cast.append(" ");
+            cast.append(actor.getLastName());
+            cast.append(" ");
+        }
+        return cast.toString();
+    }
+
+    public void setActors(String actors) {
+        this.actors = actors;
+    }
+
+    public String getCategory() {
+        Category category = helper.getCategoryID(selected.getFilmId());
+        
+        return category.getName();
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
     
-    
+    public String prepareView(){
+        selected = (Film) getFilmTitles().getRowData();
+        return "browse";
+    }
     
 }
--- src/java/dvdrental/FilmHelper.java
+++ src/java/dvdrental/FilmHelper.java
@@ -1,4 +1,3 @@
-
 package dvdrental;
 
 import org.hibernate.SQLQuery;
@@ -10,67 +9,177 @@ import java.util.List;
  * @author Matt
  */
 public class FilmHelper {
-    
+
     Session session = null;
-    
+
     public FilmHelper() {
         try {
             this.session = HibernateUtil.getSessionFactory().openSession();
             session.beginTransaction();
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
-    public List getFilmTitles (int startId) {
+
+    public List getFilmTitles(int startId) {
         List<Film> filmList = null;
-        
+
         String sql = "select * from film order by title limit :start, :end";
-        
+
         try {
-            
-            if(!this.session.getTransaction().isActive()){
-            session.beginTransaction();
-        }
-        //creating query that will be executed against the database
-        SQLQuery q = session.createSQLQuery(sql);
-        
-        q.addEntity(Film.class);
-        
-        q.setParameter("start", startId);
-        q.setParameter("end", 10);
-        
-        filmList = (List<Film>) q.list();
-        
-        }  catch (Exception e) {
+
+            if (!this.session.getTransaction().isActive()) {
+                session.beginTransaction();
+            }
+            //creating query that will be executed against the database
+            SQLQuery q = session.createSQLQuery(sql);
+
+            q.addEntity(Film.class);
+
+            q.setParameter("start", startId);
+            q.setParameter("end", 10);
+
+            filmList = (List<Film>) q.list();
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         return filmList;
     }
-    
-    public int getNumberFilms(){
-         List<Film> filmList = null;
-        
+
+    public int getNumberFilms() {
+        List<Film> filmList = null;
+
         String sql = "select * from film";
+
+        try {
+
+            if (!this.session.getTransaction().isActive()) {
+                session.beginTransaction();
+            }
+            //creating query that will be executed against the database
+            SQLQuery q = session.createSQLQuery(sql);
+
+            q.addEntity(Film.class);
+
+            filmList = (List<Film>) q.list();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return filmList.size();
+    }
+
+    public List getActorsByID(int filmId) {
+        List<Actor> actorList = null;
+
+        String sql = "select * from actor, film_actor, film "
+                + "where actor.actor_id = film_actor.actor_id "
+                + "and film_actor.film_id = film.film_id "
+                + "and film.film_id = :id";
+
+        try {
+
+            if (!this.session.getTransaction().isActive()) {
+                session.beginTransaction();
+            }
+            //creating query that will be executed against the database
+            SQLQuery q = session.createSQLQuery(sql);
+
+            q.addEntity(Actor.class);
+
+            q.setParameter("id", filmId);
+
+            actorList = (List<Actor>) q.list();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return actorList;
+    }
+    
+    public Category getCategoryID (int filmId) {
+        List<Category> categoryList = null;
+        
+        String sql = "select * from category, film_category, film "
+                + "where category.category_id = film_category.category_id "
+                + "and film_category.film_id = film.film_id "
+                + "and film.film_id = :id";
         
         try {
-            
-            if(!this.session.getTransaction().isActive()){
-            session.beginTransaction();
-        }
-        //creating query that will be executed against the database
-        SQLQuery q = session.createSQLQuery(sql);
-        
-        q.addEntity(Film.class);
-        
-        filmList = (List<Film>) q.list();
-        
-        }  catch (Exception e) {
+
+            if (!this.session.getTransaction().isActive()) {
+                session.beginTransaction();
+            }
+            //creating query that will be executed against the database
+            SQLQuery q = session.createSQLQuery(sql);
+
+            q.addEntity(Category.class);
+
+            q.setParameter("id", filmId);
+
+            categoryList = (List<Category>) q.list();
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
         
-        return filmList.size();
+        return categoryList.get(0);
     }
     
+    public Film getFilmDetails (int filmId) {
+        Film film = null;
+        
+        String sql = "select * from film where film_id = :id";
+        
+        try {
+
+            if (!this.session.getTransaction().isActive()) {
+                session.beginTransaction();
+            }
+            //creating query that will be executed against the database
+            SQLQuery q = session.createSQLQuery(sql);
+
+            q.addEntity(Film.class);
+
+            q.setParameter("id", filmId);
+
+            film = (Film) q.uniqueResult();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return film;
+    }
+    
+    public String getLangByID (int langId){
+        Language language = null;
+        
+        String sql = "select * from language where language_id = :id";
+        
+        try {
+
+            if (!this.session.getTransaction().isActive()) {
+                session.beginTransaction();
+            }
+            //creating query that will be executed against the database
+            SQLQuery q = session.createSQLQuery(sql);
+
+            q.addEntity(Language.class);
+
+            q.setParameter("id", langId);
+
+            language = (Language) q.uniqueResult();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return language.getName();
+    }
+
 }
